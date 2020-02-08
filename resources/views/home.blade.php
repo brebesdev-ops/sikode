78078bb406742868d16d0117d51094c705c17f80
brebesdev-ops <brebesdev@gmail.com>
2020-02-08 21:52:16 +0700

    commit no bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Sikode | Cirebonese Software Developer')
 
 @section('content')
+
 <section>
     <div class="text-center featured-area light-bg padding-big" id=feature>
         <div class=container>
@@ -39,78 +40,8 @@
         </div>
     </div>
 </section>
-<!-- <section>
-        <div class="padding-big app-overview-area" id=overview>
-            <div class=container>
-                <div class="row text-center">
-                    <div class="col-sm-8 col-sm-offset-2">
-                        <div class="section-heading mb-40">
-                            <h2 class="section-title title-border">Company Overview</h2>
-                            <p>We collaborate together will other powerful companies in maximizing our productivity. Take a look !</div>
-                    </div>
-                </div>
-                <div class="row flexbox-center xs-no-flexbox">
-                    <div class=col-md-4>
-                        <div class="app-overview-lists app-lists-left text-right">
-                            <ul>
-                                <li class=clearfix>
-                                    <div class="pull-right app-overview-icon"><i class="icofont circled-icon icofont-alarm"></i></div>
-                                    <div class="pull-left app-overview-content">
-                                        <h3>Smart Notification</h3>
-                                        <p>Lorem ipsum dolor amet consectetur adipisicing eiusmod.</div>
-                                    <li class=clearfix>
-                                        <div class="pull-right app-overview-icon"><i class="icofont circled-icon icofont-battery-full"></i></div>
-                                        <div class="pull-left app-overview-content">
-                                            <h3>Bettery Saver</h3>
-                                            <p>Lorem ipsum dolor amet consectetur adipisicing eiusmod.</div>
-                                        <li class=clearfix>
-                                            <div class="pull-right app-overview-icon"><i class="icofont circled-icon icofont-check"></i></div>
-                                            <div class="pull-left app-overview-content">
-                                                <h3>Virus Protected</h3>
-                                                <p>Lorem ipsum dolor amet consectetur adipisicing eiusmod.</div>
-                                            <li class=clearfix>
-                                                <div class="pull-right app-overview-icon"><i class="icofont circled-icon icofont-dashboard"></i></div>
-                                                <div class="pull-left app-overview-content">
-                                                    <h3>Fast Performance</h3>
-                                                    <p>Lorem ipsum dolor amet consectetur adipisicing eiusmod.</div>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class=col-md-4>
-                        <div class="text-center app-overiew-photo pb-45 pt-45"><img alt=iPhone src=img/demo-1/iphone.png class="center-block img-responsive"></div>
-                    </div>
-                    <div class=col-md-4>
-                        <div class="app-overview-lists app-lists-right">
-                            <ul>
-                                <li class=clearfix>
-                                    <div class="pull-left app-overview-icon"><i class="icofont circled-icon icofont-gears"></i></div>
-                                    <div class="pull-right app-overview-content">
-                                        <h3>Easy UX</h3>
-                                        <p>Lorem ipsum dolor amet consectetur adipisicing eiusmod.</div>
-                                    <li class=clearfix>
-                                        <div class="pull-left app-overview-icon"><i class="icofont circled-icon icofont-image"></i></div>
-                                        <div class="pull-right app-overview-content">
-                                            <h3>Clean UI Design</h3>
-                                            <p>Lorem ipsum dolor amet consectetur adipisicing eiusmod.</div>
-                                        <li class=clearfix>
-                                            <div class="pull-left app-overview-icon"><i class="icofont circled-icon icofont-share"></i></div>
-                                            <div class="pull-right app-overview-content">
-                                                <h3>Social Share</h3>
-                                                <p>Lorem ipsum dolor amet consectetur adipisicing eiusmod.</div>
-                                            <li class=clearfix>
-                                                <div class="pull-left app-overview-icon"><i class="icofont circled-icon icofont-macbook"></i></div>
-                                                <div class="pull-right app-overview-content">
-                                                    <h3>Device Control</h3>
-                                                    <p>Lorem ipsum dolor amet consectetur adipisicing eiusmod.</div>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
 <section>
-    <div class="padding-big screenshots-area" id=team>
+    <div class="padding-big screenshots-area " id=team>
         <div class=container>
             <div class="row text-center">
                 <div class="col-sm-8 col-sm-offset-2">
@@ -181,13 +112,13 @@
                         <div class="team-overlay"></div>
                         <!-- Start Person Photo -->
                         <div class="pic">
-                            <img src="assets/img/team/didin.jpeg" alt="Applora" class="img-responsive">
+                            <img src="assets/img/team/fajar.jpg" alt="Applora" class="img-responsive">
                         </div>
                         <!-- End Person Photo -->
                         <!-- Start Person Info -->
                         <div class="team-prof">
-                            <h3 class="post-title">Didin Afifudin </h3>
-                            <span class="post">UX/UI Designer | Front-End</span>
+                            <h3 class="post-title">Bernadus Fajar </h3>
+                            <span class="post">Dev-Ops| FullStack Developer</span>
                             <!-- Start Person Social -->
                             <div class="social-team text-center">
                                 <span class="applora-social-links"><a href="#"><i class='fa fa-facebook'></i></a></span>
@@ -200,7 +131,7 @@
                     </div>
                 </div>
                 <!-- End Single Person -->
-                
+
 
                 <!-- Start Single Person -->
                 <div class="col-md-3 col-sm-6 fix-width-right wow fadeInUp" data-wow-delay="0.8s"
@@ -229,7 +160,71 @@
                 </div>
                 <!-- End Single Person -->
             </div>
-          
+
+            <div class="row">
+                <!-- Start Single Person -->
+                <div class="col-md-3 col-sm-6 fix-width-left wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1s">
+                    
+                </div>
+                <!-- End Single Person -->
+
+                <!-- Start Single Person -->
+                <div class="col-md-3 col-sm-6 fix-width-right wow fadeInUp" data-wow-delay="0.4s"
+                    data-wow-duration="1s">
+                    <div class="team">
+                        <div class="team-overlay"></div>
+                        <!-- Start Person Photo -->
+                        <div class="pic">
+                            <img src="assets/img/team/haidar.jpeg" alt="Applora" class="img-responsive">
+                        </div>
+                        <!-- End Person Photo -->
+                        <!-- Start Person Info -->
+                        <div class="team-prof">
+                            <h3 class="post-title">Haidar Fakhri</h3>
+                            <span class="post">Front-End Developer </span>
+                            <!-- Start Person Social -->
+                            <div class="social-team text-center">
+                                <span class="applora-social-links"><a href="#"><i class='fa fa-facebook'></i></a></span>
+                                <span class="applora-social-links"><a href="#"><i class='fa fa-twitter'></i></a></span>
+                                <span class="applora-social-links"><a href="#"><i class='fa fa-behance'></i></a></span>
+                            </div>
+                            <!-- End Person Social -->
+                        </div>
+                        <!-- End Person Info -->
+                    </div>
+                </div>
+                <!-- End Single Person -->
+
+                <!-- Start Single Person -->
+                <div class="col-md-3 col-sm-6 fix-width-left wow fadeInUp" data-wow-delay="0.6s" data-wow-duration="1s">
+                    <div class="team">
+                        <div class="team-overlay"></div>
+                        <!-- Start Person Photo -->
+                        <div class="pic">
+                            <img src="assets/img/team/yuna.jpeg" alt="Applora" class="img-responsive">
+                        </div>
+                        <!-- End Person Photo -->
+                        <!-- Start Person Info -->
+                        <div class="team-prof">
+                            <h3 class="post-title">Yuna Mardiana</h3>
+                            <span class="post">Back-End Developer</span>
+                            <!-- Start Person Social -->
+                            <div class="social-team text-center">
+                                <span class="applora-social-links"><a href="#"><i class='fa fa-facebook'></i></a></span>
+                                <span class="applora-social-links"><a href="#"><i class='fa fa-twitter'></i></a></span>
+                                <span class="applora-social-links"><a href="#"><i class='fa fa-behance'></i></a></span>
+                            </div>
+                            <!-- End Person Social -->
+                        </div>
+                        <!-- End Person Info -->
+                    </div>
+                </div>
+                <!-- End Single Person -->
+
+
+        
+            </div>
+
         </div>
     </div>
 </section>
@@ -264,116 +259,6 @@
                 class="icofont circled-icon icofont-image"></i></a></div>
     </div>
 </section>
-<!-- <section>
-    <div class="padding-big screenshots-area" id=client>
-        <div class=container>
-            <div class="row text-center">
-                <div class="col-sm-8 col-sm-offset-2">
-                    <div class="section-heading mb-65">
-                        <h2 class="section-title title-border">Meet Our clients</h2>
-                        <p>What Our Clients Are Saying? Many of our clients provide excellent constructive feedbacks as
-                            our delivarables fullfil their requirements. So will you be the next?
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-3 col-sm-6 fix-width-left wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1s">
-                    <div class="team">
-                        <div class="team-overlay"></div>
-                        <div class="pic">
-                            <img src="http://kukuandroid.esy.es/images/p1.jpg" alt="Applora" class="img-responsive">
-                        </div>
-            
-                        <div class="team-prof">
-                            <h3 class="post-title">Wan M.Faizal,CEO of WMF Solution</h3>
-                            <span class="post">“We used KukuBytes to develop an Android app for tablets and smartphones
-                                and they made an incredible job including new tips that make the app even better. Now we
-                                started new project for iOS and Android and few others in pipeline. After working with
-                                several mobile application development companies, we finally found our real development
-                                studio partner for the long run. ”
-
-                            </span>
-
-                            <div class="social-team text-center">
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-facebook'></i></a></span>
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-twitter'></i></a></span>
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-behance'></i></a></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3 col-sm-6 fix-width-right wow fadeInUp" data-wow-delay="0.4s"
-                    data-wow-duration="1s">
-                    <div class="team">
-                        <div class="team-overlay"></div>
-                        <div class="pic">
-                            <img src="http://kukuandroid.esy.es/images/p2.jpg" alt="Applora" class="img-responsive">
-                        </div>
-                        <div class="team-prof">
-                            <h3 class="post-title">Megat Daharus Megat Idris, Freelancer </h3>
-                            <span class="post">“ After failing to outsource our Android application development to
-                                several companies we found KukuBytes. They are professionals and have finished our
-                                project on time and on budget. If you're looking for a top android app development
-                                company, go with KukuBytes.”
-
-                
-
-                            </span>
-                            <div class="social-team text-center">
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-facebook'></i></a></span>
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-twitter'></i></a></span>
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-behance'></i></a></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3 col-sm-6 fix-width-left wow fadeInUp" data-wow-delay="0.6s" data-wow-duration="1s">
-                    <div class="team">
-                        <div class="team-overlay"></div>
-                        <div class="pic">
-                            <img src="http://kukuandroid.esy.es/images/p3.jpg" alt="Applora" class="img-responsive">
-                        </div>
-
-                        <div class="team-prof">
-                            <h3 class="post-title">Nur Shahira Zahari,Business Woman</h3>
-                            <span class="post">KukuBytes was great, they assist me from the root stage of developing app
-                                until the process of launching it. Thank you so much ”</span>
-                            <div class="social-team text-center">
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-facebook'></i></a></span>
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-twitter'></i></a></span>
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-behance'></i></a></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3 col-sm-6 fix-width-right wow fadeInUp" data-wow-delay="0.8s"
-                    data-wow-duration="1s">
-                    <div class="team">
-                        <div class="team-overlay"></div>
-                        <div class="pic">
-                            <img src="img/team/img-4.jpg" alt="Applora" class="img-responsive">
-                        </div>
- 
-                        <div class="team-prof">
-                            <h3 class="post-title">Aliff Asyraf</h3>
-                            <span class="post">Marketing | Management</span>
-                            <div class="social-team text-center">
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-facebook'></i></a></span>
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-twitter'></i></a></span>
-                                <span class="applora-social-links"><a href="#"><i class='fa fa-behance'></i></a></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
 
 <section id="contact-us" class="contact-us">
     <div class="overlay">
@@ -454,7 +339,7 @@
                                             <input name="mail_name" type="text" placeholder="Full Name" required>
                                         </div>
                                         <div class="col-md-6">
-                                            <input name="mail_email"type="email" placeholder="E-Mail" required>
+                                            <input name="mail_email" type="email" placeholder="E-Mail" required>
                                         </div>
                                         <div class="col-md-12">
                                             <input name="mail_subject" type="text" placeholder="Subject" required>
@@ -499,15 +384,6 @@
                                 class="icofont icofont-2 icofont-ui-love "></i> in Brebes - Cirebon </p>
                     </div>
                 </div>
-                <!-- <div class=col-sm-7>
-                    <div class=footer-right>
-                        <ul class="pull-right footer-menu list-inline xs-no-float">
-                            <li><a href=#>Home</a>
-                            <li><a href=#>Services</a>
-                            <li><a href=#>Portfolio</a>
-                            <li><a href=#>Contact Us</a>
-                    </div>
-                </div> -->
             </div>
         </div>
     </div>
